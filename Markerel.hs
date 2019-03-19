7d75a208290aa0c0784e7041ec19262ff9c1a6e7
Connor Kuehl <cipkuehl@gmail.com>
2019-03-19 09:58:29 -0700

    Enable reading stdin for UNIX pipe-y things
    Now, with actual utility! You can invoke Markerel like so:
    
    cat mydoc.md | runghc Markerel

--- Markerel.hs
+++ Markerel.hs
@@ -1,5 +1,9 @@
 import DocGen
 import Parse
+import System.IO
 
 markerel :: String -> String
 markerel = generateHtml . parseDoc
+
+main :: IO ()
+main = getContents >>= return . markerel >>= putStrLn
