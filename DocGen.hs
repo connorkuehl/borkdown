304596362f490eb46b87ca25790522056a9c5782
Connor Kuehl <cipkuehl@gmail.com>
2019-03-19 11:58:18 -0700

    Add support for ordered & unordered lists

--- DocGen.hs
+++ DocGen.hs
@@ -4,14 +4,30 @@ import Parse
 
 generateHtml     :: Document -> String
 generateHtml doc = "<html><body>" ++ (concat . map generateParagraph) doc ++ "</body></html>"
-  where generateParagraph :: Paragraph -> String
-        generateParagraph (Heading level content) = openHeading ++ content ++ closeHeading
-            where openHeading = "<h" ++ show level ++ ">"
-                  closeHeading = "</h" ++ show level ++ ">"
-        generateParagraph (Para inlines) = "<p>" ++ content ++ "</p>"
-            where content = (concat . map generateInline) inlines
-        generateInline (Plain s) = s
-        generateInline (Link desc url) = "<a href=" ++ url ++ ">" ++ desc ++ "</a>"
-        generateInline (Italic i) = "<em>" ++ generateInline i ++ "</em>"
-        generateInline (Bold b) = "<strong>" ++ generateInline b ++ "</strong>"
-        generateInline (Code c) = "<code>" ++ c ++ "</code>"
+
+generateParagraph :: Paragraph -> String
+generateParagraph (Heading level content) = openHeading ++ content ++ closeHeading
+    where openHeading = "<h" ++ show level ++ ">"
+          closeHeading = "</h" ++ show level ++ ">"
+generateParagraph (Para inlines) = "<p>" ++ content inlines ++ "</p>"
+generateParagraph (OrdList items) = "<ol>" ++ generateListItems items ++ "</ol>"
+generateParagraph (UnordList items) = "<ul>" ++ generateListItems items ++ "</ul>"
+
+generateListItems :: [Item] -> String
+generateListItems is = (concat . map generateListItem) is
+
+generateListItem :: Item -> String
+generateListItem (Ordered is) = listItem is
+generateListItem (Unordered is) = listItem is
+
+listItem :: [Inline] -> String
+listItem i = "<li>" ++ content i ++ "</li>"
+
+generateInline :: Inline -> String
+generateInline (Plain s) = s
+generateInline (Link desc url) = "<a href=" ++ url ++ ">" ++ desc ++ "</a>"
+generateInline (Italic i) = "<em>" ++ generateInline i ++ "</em>"
+generateInline (Bold b) = "<strong>" ++ generateInline b ++ "</strong>"
+generateInline (Code c) = "<code>" ++ c ++ "</code>"
+
+content inlines = (concat . map generateInline) inlines
--- Parse.hs
+++ Parse.hs
@@ -2,7 +2,7 @@ module Parse where
 
 import Text.Parsec
 import Text.Parsec.String (Parser)
-import Data.Char
+import Data.Char hiding (ord)
 
 type Document  = [Paragraph]
 
@@ -10,7 +10,13 @@ type Paragraph = Block
 
 data Block = Heading Int String
            | Para [Inline]
+           | OrdList [Item]
+           | UnordList [Item]
            | Blank
+  deriving (Eq, Show)
+
+data Item = Ordered [Inline]
+          | Unordered [Inline]
   deriving (Eq, Show)
 
 data Inline = Plain String
@@ -23,19 +29,28 @@ data Inline = Plain String
 -- parseDoc parses a string that represents an entire Markdown
 -- document into its block representation for document generation.
 parseDoc :: String -> Document
-parseDoc = removeBlanks . map toBlock . lines
+parseDoc = coalesce . removeBlanks . map toBlock . lines
   where
     -- Remove all other unnecessary blank lines.
     removeBlanks            :: [Block] -> [Block]
     removeBlanks []         = []
     removeBlanks (Blank:bs) = removeBlanks bs
     removeBlanks (b:bs)     = b:removeBlanks bs
+    coalesce :: [Block] -> [Block]
+    coalesce [] = []
+    coalesce (OrdList fs:OrdList ss:is) = (OrdList (fs ++ ss)):coalesce is
+    coalesce (UnordList fs:UnordList ss:is) = (UnordList (fs ++ ss)):coalesce is
+    coalesce (i:is) = i:coalesce is
 
 -- This classification function was written by Mark Jones
 -- under the name "classify". I've extended it to recursively
 -- descend and produce inline elements.
 toBlock :: String -> Block
 toBlock line | all isSpace line = Blank
+             | isOrd line = case (parse ord "" line) of
+                              Right s -> OrdList ([Ordered (getItem line s)])
+             | isUnord line = case (parse unOrd "" line) of
+                              Right s -> UnordList ([Unordered (getItem line s)])
              | otherwise = case span ('#'==) line of
                              ([], s) -> Para ((compress . inlines) s)
                              (hs, s) -> Heading (length hs) (dropWhile isSpace s)
@@ -47,6 +62,13 @@ toBlock line | all isSpace line = Blank
     compress []                   = []
     compress (Plain s:Plain p:is) = compress (Plain (s ++ p):is)
     compress (i:is)               = i:compress is
+    isOrd l = case (parse ord "" l) of
+                Left _ -> False
+                Right _ -> True
+    isUnord l = case (parse unOrd "" l) of
+                  Left _ -> False
+                  Right _ -> True
+    getItem line lead = (compress . inlines) (drop (length lead) line)
 
 -- inlines recursively parses the contents of a paragraph block and
 -- produces any inline elements that it fines (links, bold, italics,
@@ -105,3 +127,14 @@ link = do desc <- between (char '[') (char ']') (manyTill anyChar (lookAhead (ch
 
 code :: Parser Inline
 code = Code <$> between (char '`') (char '`') (manyTill anyChar (lookAhead (char '`')))
+
+ord :: Parser String
+ord = do num <- many1 digit
+         dot <- char '.'
+         space <- char ' '
+         return $ num ++ [dot] ++ [space]
+
+unOrd :: Parser String
+unOrd = do star <- char '*'
+           space <- char ' '
+           return $ [star] ++ [space]
